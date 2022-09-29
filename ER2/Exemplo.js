73cf7c1011382831cc6f7a1e1fb003693e254848
fariagui <guilafaria@gmail.com>
2022-09-29 14:01:26 -0300

    atividade encontro remoto 2

---
+++ ER2/Exemplo.js
@@ -0,0 +1,13 @@
+// while significa enquanto.
+/*
+Existem dois tipos de laços, contados e laços com variaveis de controle
+while pode ser utilizsdo como contador e como variavel de controle 
+*/
+//whie com contador
+let contador = 0; //contador iniciando no 0
+
+while (contador < 100)//precisa de uma condição  
+{
+    console.log (contador)
+    contador = contador + 1; //incremento.
+}
---
+++ ER2/encontroremoto2.js
@@ -0,0 +1,14 @@
+let numerodeAlunos = 10;
+
+for(let contador = 0; contador <= numerodeAlunos; contador++) {
+       
+}
+if (contador == 0){
+    console.log(`${contador} - zero` )// INTERPOLAÇÃO
+}
+else if ((contador % 2 )==0){
+    console.log(`${contador} - Par` )
+}
+else {
+    console.log(`${contador} - Impar`)
+}
